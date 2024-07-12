004d794fbda329156284c210625b2d1abac522cf
Luis Carlos <carlos@charro.digital>
2024-07-12 14:26:47 -0600

    Remove seats routes

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,9 @@ const router = express.Router();
 const bookerRoutes = require('./bookers');
 const bookingRoutes = require('./bookings');
 const auditoriumRoutes = require('./auditoriums');
-const seatRoutes = require('./seats');
 
 router.use('/v1/bookers', bookerRoutes);
 router.use('/v1/bookings', bookingRoutes);
 router.use('/v1/auditoriums', auditoriumRoutes);
-router.use('/v1/seats', seatRoutes);
 
 module.exports = router;
