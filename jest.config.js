4f4f46e524afa48375bfae7813d32321278cbd3b
Luis Carlos <carlos@charro.digital>
2024-07-12 22:06:00 -0600

    Changes for jest config

--- jest.config.js
+++ jest.config.js
@@ -195,9 +195,6 @@ const config = {
   // watchman: true,
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./tests/jest.setup.js'],
-  // testPathIgnorePatterns: ['/node_modules/'],
-  // globalSetup: './tests/globalSetup.js',
-  // globalTeardown: './tests/globalTeardown.js',
   testMatch: ['**/tests/**/*.test.js'],
   maxWorkers: 1,
 };
